ed89ceb39f2577d27353a8c6154aa5646ec2b5cd
Andrew Timkov <andrew.timkov@gmail.com>
2020-12-15 21:55:40 +1100

    Finished day 15

--- 15.py
+++ 15.py
@@ -1,8 +1,21 @@
 with open('15.txt', 'r') as f:
-    data = [l.rstrip() for l in f.readlines()]
+    memory = {int(e):i+1 for i, e in enumerate(n for n in f.readline().rstrip().split(','))}
 
-part_1 = 0
-part_2 = 0
+for k in memory:
+    if memory[k] == len(memory):
+        prev = k
+        break
+
+for i in range(len(memory)+1, 30000001):
+    if prev not in memory:
+        diff = 0
+    else:
+        diff = i - 1 - memory[prev]
+    memory[prev] = i-1
+    prev = diff
+    if i == 2020:
+        part_1 = prev
+part_2 = prev
 
 print("Part 1:", part_1)
 print("Part 2:", part_2)
